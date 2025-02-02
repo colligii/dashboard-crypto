7019efc56061c6a3c773b7852cb6285dee05d473
Giovanne Colli <cgiovanne45@gmail.com>
2025-02-02 10:16:22 -0300

    finish header and add icon sizes

--- src/app/shared/header.tsx
+++ src/app/shared/header.tsx
@@ -1,5 +1,10 @@
+import Image from "./sources/img/logo-sm.png"
 export default function Header() {
-    return <header className="w-screen p-2">
-        Header
+    return <header className="w-screen p-2 flex gap-2 items-center justify-center">
+        <img src={Image.src} width={50} height={50}/>
+        <div className="flex flex-col items-center justify-center pt-4">
+            <span className="leading-[12px]">Mercado</span>
+            <span className="text-lg font-black leading-[22px]">Bull Run</span>
+        </div>
     </header>
 }
