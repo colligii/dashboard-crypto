9ffcef85b18aa4e2d90d3f9bd960c3f3327972c6
Giovanne Colli <cgiovanne45@gmail.com>
2025-02-02 10:48:56 -0300

    add react-window

---
+++ src/app/components/assets-list.tsx
@@ -0,0 +1,11 @@
+import { AssetsFetch } from "../hooks/getAssets"
+
+export default function AssetsList({ assets }: AssetsListProps) {
+    return (
+        <div>teste</div>
+    )
+}
+
+export type AssetsListProps = {
+    assets: AssetsFetch[]
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import useGetAssets from "./hooks/getAssets";
 import HooksError from "./utils/hooks-error";
 import BearImage from "./shared/sources/img/bear/bear-md.png";
+import {VariableSizeList} from "react-window"
+import AssetsList from "./components/assets-list";
 
 declare var process: any;
 
@@ -17,6 +19,7 @@ export default async function Home() {
                     <p className="font-bold text-lg">{crypto.message}</p>
                 </div> :
                 <div>
+                    <AssetsList/>
                     {crypto.map(item => item.symbol)}
                 </div>
             }
