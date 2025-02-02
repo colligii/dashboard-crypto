f540c3abbb6a924673b092212401cbb4109a88e4
Giovanne Colli <cgiovanne45@gmail.com>
2025-02-02 10:28:23 -0300

    add error img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import useGetAssets from "./hooks/getAssets";
 import HooksError from "./utils/hooks-error";
+import BearImage from "./shared/sources/img/bear/bear-md.png";
 
 declare var process: any;
 
@@ -13,8 +14,9 @@ export default async function Home() {
         <>
             {
                 crypto instanceof HooksError ?
-                <div className="w-screen h-screen flex items-center justify-center">
-                    {crypto.message}
+                <div className="w-screen h-screen flex flex-col items-center justify-center">
+                    <img src={BearImage.src} width={200} height={200}/>
+                    <p className="font-bold text-lg">{crypto.message}</p>
                 </div> :
                 <div>
                     Lista
