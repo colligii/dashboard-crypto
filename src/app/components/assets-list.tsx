0a6925ddfed230cbfa9c93122ad94821837bd6a8
Giovanne Colli <cgiovanne45@gmail.com>
2025-02-06 19:41:19 -0300

    remove comments

--- src/app/components/assets-list.tsx
+++ src/app/components/assets-list.tsx
@@ -20,8 +20,7 @@ export default function AssetsList({ assets }: AssetsListProps) {
             }
         };
 
-        updateSize(); // Chama na montagem para pegar os valores iniciais
-
+        updateSize(); 
         window.addEventListener("resize", updateSize);
         return () => window.removeEventListener("resize", updateSize);
     }, []);
