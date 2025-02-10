195c6af4d1b492e6f106e5a798fdef3cbab729a5
Giovanne Colli <cgiovanne45@gmail.com>
2025-02-09 22:31:45 -0300

    start useSWR with refactor

--- src/app/crypto/[id]/page.tsx
+++ src/app/crypto/[id]/page.tsx
@@ -1,34 +1,41 @@
-import useGetAssetsById from "@/app/hooks/getAssetById"
-import HooksError from "@/app/utils/hooks-error";
+"use client"
+
+import { use } from "react";
 import useSWR from "swr";
 
-const fetcher = (...args: any) => fetch(...args).then(res => res.json())
+const fetcher = (url: string) => {
+    console.log(url)
+    return fetch(url).then(res => res.json())
+};
 
-export default async function CryptoPage({ params }: any) {
+export default function CryptoPage({ params }: any) {
 
-    const { id } = await params;
-    const cryptoInfo = await useGetAssetsById(id)
-    const { data, error } = useSWR(`/api/crypto/${id}`, )
+    const _params: any = use(params);
+    const data = useSWR(`/api/crypto/${_params.id}`, fetcher, {
+        refreshInterval: 60000
+    })
 
-    if (cryptoInfo instanceof HooksError)
-        return
+    return <h1>Teste {_params.id}</h1>
 
-    return (
-        <main className="pt-[68px] p-2">
-            <div className="flex flex-wrap gap-4">
-                <div className="flex flex-col">
-                    <span className="font-bold">Name:</span>
-                    <span>{cryptoInfo.name}</span>
-                </div>
-                <div className="flex flex-col">
-                    <span className="font-bold">Price:</span>
-                    <span>{Number(cryptoInfo.priceUsd).toLocaleString("en-US", { style: "currency", currency: "USD" })}</span>
-                </div>
-                <div className="flex flex-col">
-                    <span className="font-bold">Market Cap:</span>
-                    <span>{Number(cryptoInfo.marketCapUsd).toLocaleString("en-US", { style: "currency", currency: "USD" })}</span>
-                </div>
-            </div>
-        </main>
-    )
+    // if (cryptoInfo instanceof HooksError)
+    //     return
+
+    // return (
+    //     <main className="pt-[68px] p-2">
+    //         <div className="flex flex-wrap gap-4">
+    //             <div className="flex flex-col">
+    //                 <span className="font-bold">Name:</span>
+    //                 <span>{cryptoInfo.name}</span>
+    //             </div>
+    //             <div className="flex flex-col">
+    //                 <span className="font-bold">Price:</span>
+    //                 <span>{Number(cryptoInfo.priceUsd).toLocaleString("en-US", { style: "currency", currency: "USD" })}</span>
+    //             </div>
+    //             <div className="flex flex-col">
+    //                 <span className="font-bold">Market Cap:</span>
+    //                 <span>{Number(cryptoInfo.marketCapUsd).toLocaleString("en-US", { style: "currency", currency: "USD" })}</span>
+    //             </div>
+    //         </div>
+    //     </main>
+    // )
 }
